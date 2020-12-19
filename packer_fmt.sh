4bb16eedc3126047d74a9a83e0d422f1c705dc2b
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-12-19 15:27:22 -0500

    Initial hook additions
    Adds two hooks and a suitable .pre-commit-hooks.yaml file. The two hooks will
    provide automation to run `packer fmt -check` and `packer validate` on either
    matching or manually specified files.

---
+++ packer_fmt.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if [ -z "$(command -v packer)" ]
+then
+  echo "packer is required"
+  exit 1
+fi
+
+error=0
+
+for file in "$@"
+do
+  if ! packer fmt -check "$file"
+  then
+    error=1
+    echo
+    echo "Failed path: $file"
+    echo "================================"
+  fi
+done
+
+if [[ $error -ne 0 ]]
+then
+    exit 1
+fi
